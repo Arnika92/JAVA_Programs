d213fc9cf80248f48473067e97c01cbe5d47d1fd
Arnika92 <128769521+Arnika92@users.noreply.github.com>
2023-04-04 13:47:23 +0530

    Finding index

---
+++ Indexarr.java
@@ -0,0 +1,15 @@
+package Day7;
+import java.util.Scanner;
+public class Indexarr {
+  public static void main(String[] args) {
+	  Scanner sc= new Scanner(System.in);
+	  System.out.println("Enter the length of the array -");
+		int length=sc.nextInt();
+		int a[]=new int[length];
+		System.out.println("Enter the elements for the array -");
+		for(int i=0;i<a.length;i++) {
+			a[i]=sc.nextInt();
+		}
+		for()
+  }
+}
